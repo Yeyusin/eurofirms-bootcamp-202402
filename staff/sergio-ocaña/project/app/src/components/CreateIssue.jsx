5892674bbee25fddfbdcab33be0ffdb198d301da
Yeyusin <sergioocruiz@gmail.com>
2024-08-13 20:57:50 +0200

    add handle in no rooms or cinema to shows #257

--- staff/sergio-ocaña/project/app/src/components/CreateIssue.jsx
+++ staff/sergio-ocaña/project/app/src/components/CreateIssue.jsx
@@ -1,4 +1,4 @@
-import { HTag, Form, LabelInput, Button, Article, ButtonText } from './index'
+import { HTag, Form, LabelInput, Button, Article, ButtonText, P } from './index'
 import { useEffect, useState } from 'react'
 import logic from '../logic'
 import { errors } from 'com'
@@ -172,9 +172,10 @@ function CreateIssue({ handleCancelButtonIssue, handleCreatedIssue }) {
                 <HTag level={3}> Select Cinema</HTag>
 
                 <ul className='flex flex-col w-full gap-1'>
-                    {cinemas.map(cinema => {
+                    {cinemas?.map(cinema => {
                         return <li
                             className={'flex cursor-pointer rounded-md p-1 bg-white text-[#4ca4b8]'} onClick={() => handleSelectedCinema(cinema.id)} key={cinema.id}> {cinema.name}
+                            {rooms.length === 0 && <P>No cinema avalaible to asign</P>}
                         </li>
                     })}
                 </ul >
@@ -199,6 +200,7 @@ function CreateIssue({ handleCancelButtonIssue, handleCreatedIssue }) {
                             className={'flex cursor-pointer bg-white text-[#4ca4b8] w-full rounded-md p-1'} onClick={() => handleSelectedRoom(room.id, room.name)} key={room.id}> {room.name}
                         </li>
                     })}
+                    {rooms.length === 0 && <P>No rooms avalaible to asign</P>}
                 </ul >
             </>}
 
